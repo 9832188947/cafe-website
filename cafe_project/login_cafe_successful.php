aa029f6cbf8cdbaf69162a70385e3c2e348185f1
9832188947 <166895361+9832188947@users.noreply.github.com>
2024-04-23 13:26:55 +0530

    Add files via upload

---
+++ cafe_project/login_cafe_successful.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+
+$email=$_POST['un'];
+$pass=$_POST['pwd'];
+
+$host="localhost";
+$user="root";
+$password="";
+$db="cafe";
+
+$conn= mysqli_connect($host,$user,$password,$db) or die("Could not connect to database");
+
+$sql="select*from cafe_data where email_id='$email'and password='$pass'";
+
+$result=mysqli_query($conn,$sql) or die("could not execute query");
+
+if(mysqli_num_rows($result)>0)
+{
+//echo "<font color='green'size='5'>Sucess</font>";
+header("location:http://localhost/cafe_project/home%20page%20of%20cafe.html");
+}
+
+else
+{
+echo "<center><font color='red' size='5'>Warning Invalid email id or password</font></center>";
+}
+
+
+
+?>
+</body>
+</html>
